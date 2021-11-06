8dcedea148322ca7889b51ac790d908d6504176e
onlinesuksesabadi <feyberho@gmail.com>
2021-11-06 09:29:06 +0700

    .

--- pages/tentang.js
+++ pages/tentang.js
@@ -44,7 +44,7 @@ export default function Tentang() {
           VISI
         </div>
       </div>
-      <section className=" relative mt-8 p-20 md:grid-cols-3 md:grid-rows-2 items-center justify-between md:justify-center lg:gap-4 rounded-xl bg-red-500 grid lg:grid-rows-2 lg:grid-cols-6 grid-rows-6 grid-cols-2 sm:flex-col">
+      <section className=" relative mt-8 p-20 md:grid-cols-3 md:grid-rows-2 items-center justify-around md:justify-center gap-2 lg:gap-4 rounded-xl bg-red-500 grid lg:grid-rows-2 lg:grid-cols-6 grid-rows-6 grid-cols-2 sm:flex-col">
         {/* <!-- rotate --> */}
         <div className="absolute top-0 font-bold bg-indigo-600 ml-4 -mt-4 px-4 py-2 text-indigo-50 rounded-lg text-sm md:text-base">
           CLIENT KAMI
