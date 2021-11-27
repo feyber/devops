0ca47a41dba8fad339ff08aec126b683b7a31522
onlinesuksesabadi <feyberho@gmail.com>
2021-11-27 13:47:28 +0700

    rubah width heigth animasi

--- components/hero.js
+++ components/hero.js
@@ -82,9 +82,9 @@ export default function Hero() {
 
       <Container className="flex flex-wrap w-11/12 mt-10 lg:mt-0 mb-4">
         <div className="relative flex items-center w-full lg:w-1/2 ">
-          <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 mix-blend-multiply filter blur-xl opacity-50 rounded-full animate-blob mt-10 z-0"></div>
-          <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 mix-blend-multiply filter blur-xl opacity-50 rounded-full animate-blob animation-delay-2000 mt-20 z-0"></div>
-          <div className="absolute top-0 -bottom-8 w-72 h-72 bg-pink-300 mix-blend-multiply filter blur-xl opacity-50 rounded-full animate-blob animation-delay-4000 mt-36 z-0"></div>
+          <div className="absolute top-0 -left-4 w-52 h-52 md:w-72 md:h-72 bg-purple-300 mix-blend-multiply filter blur-xl opacity-50 rounded-full animate-blob mt-10 z-0"></div>
+          <div className="absolute top-0 -right-4 w-52 h-52 md:w-72 md:h-72 bg-yellow-300 mix-blend-multiply filter blur-xl opacity-50 rounded-full animate-blob animation-delay-2000 mt-20 z-0"></div>
+          <div className="absolute top-0 -bottom-8 w-52 h-52 md:w-72 md:h-72 bg-pink-300 mix-blend-multiply filter blur-xl opacity-50 rounded-full animate-blob animation-delay-4000 mt-36 z-0"></div>
 
           <div className="max-w-2xl mb-8 z-10">
             {/* <h1 className="text-2xl font-bold leading-snug tracking-tight text-gray-800 lg:text-3xl lg:leading-tight xl:text-4xl xl:leading-tight dark:text-white">
